86cd06eac7e20a8f89baf38c0ec1bc24c6d3a709
Khaled <khaled@MacBook-Pro-Amina.local>
2023-12-31 13:33:09 +0500

    Create Card and CardsList components

--- lesson-1/src/app/app.module.ts
+++ lesson-1/src/app/app.module.ts
@@ -3,15 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardListComponent } from './card-list/card-list.component';
+import { CardComponent } from './card/card.component';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardListComponent,
+    CardComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        FormsModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ lesson-1/src/app/card-list/card-list.component.ts
@@ -0,0 +1,10 @@
+import {Component, Input} from '@angular/core';
+
+@Component({
+  selector: 'app-card-list',
+  templateUrl: './card-list.component.html',
+  styleUrl: './card-list.component.css'
+})
+export class CardListComponent {
+@Input() todoList: any;
+}
